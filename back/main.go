5072c7948e0b46781516a0ab6048ca210e2a731d
diego <diegofbacag@gmail.com>
2025-08-14 21:37:15 -0500

    chore: prepare test route

--- back/main.go
+++ back/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    fmt.Fprint(w, `{"message": First route!}`)
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+    http.HandleFunc("/hello", helloHandler)
+
+    fmt.Println("Server running on http:// localhost:3000")
+    err:= http.ListenAndServe(":3000", nil)
+    if err != nil{
+        fmt.Println("Error starting server:", err)
+    }
 }

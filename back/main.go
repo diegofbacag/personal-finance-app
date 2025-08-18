0cfbad6e62b4508f52d28dd6852c50de123ec2f2
diego <diegofbacag@gmail.com>
2025-08-18 13:16:11 -0500

    chore: create expenses routes

--- back/main.go
+++ back/main.go
@@ -3,17 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    fmt.Fprint(w, `{"message": First route!}`)
+func expensesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+    parts := strings.Split(r.URL.Path,"/")
+
+
+	switch r.Method {
+	case http.MethodGet:
+		// Handle GET request
+		fmt.Fprint(w, `{"message": "All the exxpenses"}`)
+
+	case http.MethodPost:
+		// Handle POST request
+		fmt.Fprint(w, `{"message": "New expense created"}`)
+
+    case http.MethodPut:
+        // Handle PUT request
+        fmt.Fprint(w, `{"message": "Expense modified}`)
+
+    case http.MethodDelete:
+        // Handle DELETE request
+        fmt.Fprint(w, `{"message": "Expense deleted}`)
+
+	default:
+		// Handle unsupported methods
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func main() {
-    http.HandleFunc("/hello", helloHandler)
+    http.HandleFunc("/expenses", expensesHandler)
 
-    fmt.Println("Server running on http:// localhost:3000")
+    fmt.Println("Server running on http://localhost:3000")
     err:= http.ListenAndServe(":3000", nil)
     if err != nil{
         fmt.Println("Error starting server:", err)

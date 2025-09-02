c25ca69f734a521116534ff75af88821bbd7d65b
diego <diegofbacag@gmail.com>
2025-09-02 11:05:32 -0500

    chore: implemented chi as router

--- back/main.go
+++ back/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
 	"github.com/diegofbacag/personal-finance-app/back/internal/auth"
 	"github.com/diegofbacag/personal-finance-app/back/internal/expense"
 	"github.com/diegofbacag/personal-finance-app/back/internal/summary"
@@ -11,13 +14,17 @@ import (
 
 func main() {
 
-	http.HandleFunc("/auth", auth.AuthHandler)
-	http.HandleFunc("/expense", expense.ExpenseHandler)
-	http.HandleFunc("/summary", summary.SummaryHandler)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.StripSlashes)
+
+	r.Get("/auth", auth.AuthHandler)
+	r.Get("/expense", expense.ExpenseHandler)
+	r.Get("/summary", summary.SummaryHandler)
 
 	fmt.Println("Server running at http://localhost:8080")
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 	fmt.Println("Server failed:", err)
 	}

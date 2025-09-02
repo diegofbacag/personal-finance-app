074b998d779f8893be646915b87620b05df4da0f
diego <diegofbacag@gmail.com>
2025-09-02 10:46:52 -0500

    chore: set up path and route handlers in main

--- back/cmd/server/main.go
+++ back/cmd/server/main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/diegofbacag/personal-finance-app/back/internal/auth"
+	"github.com/diegofbacag/personal-finance-app/back/internal/expense"
+	"github.com/diegofbacag/personal-finance-app/back/internal/summary"
 )
-
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprintln(w, "Hello from handler!")
-
-}
-
 
 func main() {
 
-	http.HandleFunc("/hello", HelloHandler)
-
+	http.HandleFunc("/auth", auth.AuthHandler)
+	http.HandleFunc("/expense", expense.ExpenseHandler)
+	http.HandleFunc("/summary", summary.SummaryHandler)
 
 	fmt.Println("Server running at http://localhost:8080")
 

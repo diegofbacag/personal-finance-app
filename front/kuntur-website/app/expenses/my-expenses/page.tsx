c435254786c3d9baca6bf7f500e24de042535de4
Diego Baca <diegofbacag@gmail.com>
2025-11-01 14:31:44 -0500

    add input bar styles

--- front/kuntur-website/app/expenses/my-expenses/page.tsx
+++ front/kuntur-website/app/expenses/my-expenses/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
+import Image from 'next/image'
 
 interface Expense {
   amount: number
@@ -40,6 +42,8 @@ export default function MyExpensesPage() {
   })
   const [expenseHistory, setExpenseHistory] = useState<Expense[]>(expenses)
 
+  const tableEndRef = useRef<HTMLDivElement | null>(null)
+
   const handleExpenseFormInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     setExpenseFormData((prev) => ({ ...prev, [name]: value }))
@@ -50,21 +54,34 @@ export default function MyExpensesPage() {
       amount: Number(expenseFormData.amount),
       category: expenseFormData.category || undefined,
       description: expenseFormData.description || undefined,
-      date: new Date(expenseFormData.date),
+      date: new Date(`${expenseFormData.date}T00:00:00`),
     }
 
     setExpenseHistory((prev) => [...prev, newExpense])
 
-    setExpenseFormData({
+    setExpenseFormData((prev) => ({
+      ...prev,
       amount: '',
       category: '',
       description: '',
-      date: new Date().toISOString().split('T')[0],
-    })
+    }))
   }
+
+  useEffect(() => {
+    tableEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [expenseHistory])
 
   return (
     <main className="py-10 px-50 font-poppins bg-[#f5f5f5] min-h-screen pb-30">
+      <div className="fixed top-2 left-2 h-[20px] w-[20px]">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          fill="#0e8f53"
+        >
+          <path d="M32 20c0-3 2-5 5-5h9c3 0 5 2 5 5v20l20-21c1-1 2-1 3-1h10c4 0 6 5 3 8L61 50l28 31c3 3 1 9-4 9H75c-1 0-2-1-3-2L51 65v25c0 3-2 5-5 5h-9c-3 0-5-2-5-5V20z" />
+        </svg>
+      </div>
       <header className="mb-5">
         <h1 className="text-2xl font-bold">Mis gastos</h1>
       </header>
@@ -134,65 +151,127 @@ export default function MyExpensesPage() {
             })}
           </tbody>
         </table>
+        <div ref={tableEndRef} />
       </section>
 
       <section
         aria-label="Expenses input bar"
         className="fixed bottom-10 min-w-[80vw] left-1/2 transform -translate-x-1/2 "
       >
-        <div
-          className="grid grid-cols-[auto_1fr_auto] bg-white items-center border rounded-full border-gray-900/5
- p-2 shadow-md"
-        >
-          <div>+</div>
-          <div className="grid grid-cols-4 items-start text-sm text-gray">
+        <div className="grid grid-cols-[auto_1fr_auto] bg-white items-center border rounded-full border-gray-900/5 p-2 shadow-md gap-4">
+          <div className="flex items-center justify-center rounded-full text-white h-10 w-10">
+            <Image
+              src="/svg/icons/plus-bold.svg"
+              height={20}
+              width={20}
+              alt="plus icon"
+            />
+          </div>
+          <div className="grid grid-cols-5 items-start text-sm text-gray">
             <div>
-              <p>Monto S/.</p>
+              <p className="font-medium text-sm text-[#495057]">Monto S/</p>
               <input
                 type="number"
                 name="amount"
                 value={expenseFormData.amount}
                 placeholder="100"
+                min="1"
                 onChange={handleExpenseFormInput}
+                className="
+                my-1 rounded-md focus:outline-none placeholder:text-gray-400 placeholder:italic placeholder:font-light text-[#212529] bg-white transition-all duration-300 focus:bg-[#0e8f53]/10 focus:shadow-[0_0_20px_8px_rgba(14,143,83,0.12)]"
               ></input>
             </div>
             <div>
-              <p>Categoria</p>
-              <input
-                type="string"
+              <p className="font-medium text-sm text-[#495057]">Categoría</p>
+              <select
                 name="category"
                 value={expenseFormData.category}
-                placeholder="100"
                 onChange={handleExpenseFormInput}
-              ></input>
+                className="
+    my-1 rounded-md
+    focus:outline-none
+    
+    border-none
+    text-[#212529]
+    bg-white
+    transition-all duration-200
+
+    focus:shadow-[0_0_6px_1px_rgba(14,143,83,0.25)]
+    placeholder:text-gray-400
+    text-sm
+    
+  "
+              >
+                <option value="">Selecciona una subcategoría</option>
+                <option value="Gastos fijos">Gastos fijos</option>
+                <option value="Gastos libres">Gasto libre</option>
+              </select>
             </div>
             <div>
-              <p>Descripcion</p>
+              <p className="font-medium text-sm text-[#495057]">Subcategoría</p>
+              <select
+                name="subcategory"
+                value={expenseFormData.category}
+                onChange={handleExpenseFormInput}
+                className="
+    my-1 rounded-md
+    focus:outline-none
+    
+    border-none
+    text-[#212529]
+    bg-white
+    transition-all duration-200
+
+    focus:shadow-[0_0_6px_1px_rgba(14,143,83,0.25)]
+    placeholder:text-gray-400
+    text-sm
+    
+  "
+              >
+                <option value="">Selecciona una categoría</option>
+                <option value="Gastos fijos">Gastos fijos</option>
+                <option value="Gastos libres">Gasto libre</option>
+              </select>
+            </div>
+            <div>
+              <p className="font-medium text-sm text-[#495057]">Descripcion</p>
               <input
-                type="string"
+                type="text"
                 name="description"
                 value={expenseFormData.description}
-                placeholder="100"
+                placeholder="Ej. Cena con amigos"
                 onChange={handleExpenseFormInput}
-              ></input>
+                className="
+                my-1 rounded-md focus:outline-none placeholder:text-gray-400 placeholder:italic placeholder:font-light text-[#212529] bg-white transition-all duration-300 focus:bg-[#0e8f53]/10 focus:shadow-[0_0_20px_8px_rgba(14,143,83,0.12)]"
+              />
             </div>
             <div>
-              <p>Fecha</p>
+              <p className="font-medium font-medium text-[#495057]">Fecha</p>
               <input
                 type="date"
                 name="date"
-                value={expenseFormData.date}
+                value={
+                  expenseFormData.date || new Date().toISOString().split('T')[0]
+                }
                 placeholder="100"
                 onChange={handleExpenseFormInput}
-              ></input>
+                className="
+                my-1 rounded-md focus:outline-none placeholder:text-gray-400 placeholder:italic placeholder:font-light text-[#212529] bg-white transition-all duration-300 focus:bg-[#0e8f53]/10 focus:shadow-[0_0_20px_8px_rgba(14,143,83,0.12)]"
+              />
             </div>
           </div>
 
           <button
-            className="flex items-center justify-center bg-black rounded-full text-white h-10 w-10"
+            className="flex items-center justify-center bg-black rounded-full text-white h-10 w-10 cursor-pointer"
             onClick={submitExpenseFormData}
           >
-            i
+            <Image
+              src="/svg/icons/up-arrow.svg"
+              height={20}
+              width={20}
+              alt="send icon"
+              className="invert"
+            />
           </button>
         </div>
       </section>

853fa0c03bed920ec242e164672a8be7071bf3ec
Diego Baca <diegofbacag@gmail.com>
2025-11-01 12:20:01 -0500

    add table styles

--- front/kuntur-website/app/expenses/my-expenses/page.tsx
+++ front/kuntur-website/app/expenses/my-expenses/page.tsx
@@ -33,9 +33,9 @@ const expenses: Expense[] = [
 
 export default function MyExpensesPage() {
   const [expenseFormData, setExpenseFormData] = useState<ExpenseForm>({
-    amount: '',
-    category: '',
-    description: '',
+    amount: '-',
+    category: '-',
+    description: '-',
     date: new Date().toISOString().split('T')[0],
   })
   const [expenseHistory, setExpenseHistory] = useState<Expense[]>(expenses)
@@ -56,41 +56,86 @@ export default function MyExpensesPage() {
     setExpenseHistory((prev) => [...prev, newExpense])
 
     setExpenseFormData({
-      amount: '',
-      category: '',
-      description: '',
+      amount: '-',
+      category: '-',
+      description: '-',
       date: new Date().toISOString().split('T')[0],
     })
   }
 
   return (
-    <main className="p-4 font-poppins bg-[#edede9] min-h-screen pb-30">
-      <header>
-        <h1 className="text-3xl font-bold">Mis gastos</h1>
+    <main className="py-10 px-100 font-poppins bg-[#e9ecef] min-h-screen pb-30">
+      <header className="mb-5">
+        <h1 className="text-2xl font-bold">Mis gastos</h1>
       </header>
       <section aria-label="Expenses table" className="w-full">
-        <table className="w-full rounded-2xl overflow-hidden">
-          <tr className="grid grid-cols-4 h-14 items-center justify-center bg-white">
-            <th>Monto</th>
-            <th>Categoria</th>
-            <th>Descripcion</th>
-            <th>Fecha</th>
-          </tr>
-          {expenseHistory.map((e, index) => (
-            <tr
-              key={index}
-              className={`grid grid-cols-4 text-center items-center h-14 ${
-                index % 2 === 0 ? 'bg-[#edede9]' : 'bg-white'
-              }`}
-            >
-              <td>{e.amount}</td>
-              <td>{e.category}</td>
-              <td>{e.description}</td>
-              <td>{e.date.toLocaleDateString('pe-PE')}</td>
+        <table className="w-full rounded-2xl overflow-hidden border-separate border-spacing-0 text-sm text-[#212529]">
+          <thead>
+            <tr className="bg-white border-b border-[#dee2e6] text-left">
+              <th className="font-medium text-sm text-[#495057] p-4">Fecha</th>
+              <th className="font-medium text-sm text-[#495057]">
+                Descripción
+              </th>
+              <th className="font-medium text-sm text-[#495057]">Categoría</th>
+              <th className="font-medium text-sm text-[#495057]">
+                Subcategoría
+              </th>
+              <th className="font-medium text-sm text-[#495057] text-right pr-4">
+                Monto
+              </th>
             </tr>
-          ))}
+          </thead>
+
+          <tbody>
+            {expenseHistory.map((e, index) => {
+              const isGrayRow = index % 2 === 0
+              return (
+                <tr
+                  key={index}
+                  className={`
+              h-14 transition-all duration-200 text-left
+              ${
+                isGrayRow
+                  ? 'bg-[#f8f9fa] shadow-[inset_0_1px_0_rgba(0,0,0,0.03),_inset_0_-1px_0_rgba(0,0,0,0.02)]'
+                  : 'bg-white'
+              }
+              hover:shadow-[0_2px_8px_rgba(0,0,0,0.05)] hover:-translate-y-[1px] hover:bg-[#f1f3f5]
+            `}
+                >
+                  <td className="p-4">
+                    {e.date.toLocaleDateString('es-PE', {
+                      day: 'numeric',
+                      month: 'short',
+                    })}
+                  </td>
+                  <td className="truncate max-w-[200px]">
+                    {!e.description || (
+                      <span className="text-gray-400 italic">
+                        Sin descripción
+                      </span>
+                    )}
+                  </td>
+                  <td>
+                    {e.category || (
+                      <span className="text-gray-400 italic">
+                        Sin categoría
+                      </span>
+                    )}
+                  </td>
+                  <td>Fijos</td>
+                  <td className="text-right pr-4 font-medium text-[#dc3545]">
+                    {(-e.amount).toLocaleString('es-PE', {
+                      style: 'currency',
+                      currency: 'PEN',
+                    })}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
       </section>
+
       <section
         aria-label="Expenses input bar"
         className="fixed bottom-10 min-w-[80vw] left-1/2 transform -translate-x-1/2 "

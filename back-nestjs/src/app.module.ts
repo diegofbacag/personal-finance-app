b80fa9b6dbbed435a033238aaa5d640efe817195
Diego Baca <diegofbacag@gmail.com>
2025-11-11 20:22:57 -0500

    typeorm config

--- back-nestjs/src/app.module.ts
+++ back-nestjs/src/app.module.ts
@@ -3,9 +3,26 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '3000', 10),
+      username: process.env.DB_NAME,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: ['error'],
+      dropSchema: true,
+      autoLoadEntities: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

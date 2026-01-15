ad94bb76478b2c0e6b723fbb639d0f7ca24157e9
Diego Baca <diegofbacag@gmail.com>
2026-01-14 23:06:32 -0500

    small fix

--- front/kuntur-website/src/features/expenses/components/expense-input/ExpenseInputManualHorizontal.tsx
+++ front/kuntur-website/src/features/expenses/components/expense-input/ExpenseInputManualHorizontal.tsx
@@ -1,4 +1,5 @@
 import { ExpenseField } from './ExpenseField'
+import Image from 'next/image'
 import { ExpenseSelectCategory } from './ExpenseInputManualHorizontal/ExpenseSelectCategory'
 
 export const ExpenseInputManualHorizontal = () => {
@@ -16,8 +17,20 @@ export const ExpenseInputManualHorizontal = () => {
         title="Descripcion"
         name="texto"
         placeholder="Ej. Cena con amigos"
-        type="number"
+        type="text"
       />
+      <button
+        className="flex items-center justify-center bg-[#0e8f53] rounded-full text-white h-10 w-10 cursor-pointer"
+        onClick={submitExpenseFormData}
+      >
+        <Image
+          src="/svg/icons/plus-bold.svg"
+          height={20}
+          width={20}
+          alt="send icon"
+          className="invert"
+        />
+      </button>
     </div>
   )
 }

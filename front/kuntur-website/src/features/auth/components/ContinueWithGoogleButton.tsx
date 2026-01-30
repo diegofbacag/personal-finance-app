fb5bb4703327b62f3c36af372e9ac4fbbfa11190
Diego Baca <diegofbacag@gmail.com>
2026-01-30 00:31:27 -0500

    wip(auth): add redirection when clicking continue with google button

--- front/kuntur-website/src/features/auth/components/ContinueWithGoogleButton.tsx
+++ front/kuntur-website/src/features/auth/components/ContinueWithGoogleButton.tsx
@@ -1,8 +1,14 @@
 import { GoogleIcon } from '@/src/components/ui/icons/GoogleIcon'
 ///green colour #04644f
 export const ContinueWithGoogleButton = () => {
+  const handleGoogleSignIn = () => {
+    window.location.href = `http://localhost:8080/auth/google`
+  }
   return (
-    <button className="flex flex-row items-center justify-center gap-2 bg-white h-10 w-40 border rounded-xl cursor-pointer w-full">
+    <button
+      className="flex flex-row items-center justify-center gap-2 bg-white h-10 w-40 border rounded-xl cursor-pointer w-full"
+      onClick={handleGoogleSignIn}
+    >
       <GoogleIcon />
       <p className="text-black font-medium text-sm ">Continuar con Google</p>
     </button>

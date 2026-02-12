ec09ac0b56d50006e227589a37b2d420ecad60c7
Diego Baca <diegofbacag@gmail.com>
2026-02-12 12:56:14 -0500

    chore: cors config

--- back-nestjs/src/main.ts
+++ back-nestjs/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://kunturperu.vercel.app'],
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     credentials: true,
   });

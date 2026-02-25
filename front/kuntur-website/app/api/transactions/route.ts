24d65d19a53e63419d98d433905055d6924e7d0c
Diego Baca <diegofbacag@gmail.com>
2026-02-25 15:13:29 -0500

    feat(transactions): add post transaction endpoint

---
+++ front/kuntur-website/app/api/transactions/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server'
+
+import { prisma } from '@/lib/prisma'
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json()
+    const {
+      amount,
+      description,
+      date,
+      tag,
+      userId,
+      subcategoryCode,
+      categoryCode,
+    } = body
+
+    const category = await prisma.category.findUniqueOrThrow({
+      where: { code: categoryCode },
+    })
+
+    const transaction = await prisma.transaction.create({
+      data: {
+        amount,
+        description,
+        date,
+        tag,
+        user: { connect: { id: userId } },
+        subcategory: {
+          connect: {
+            code_categoryId: {
+              code: subcategoryCode,
+              categoryId: category.id,
+            },
+          },
+        },
+      },
+      include: {
+        subcategory: { include: { category: true } },
+      },
+    })
+
+    return NextResponse.json(transaction, { status: 201 })
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 })
+  }
+}

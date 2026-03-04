44bdc070064ebed3be6aaa82ac1cf6856e318c15
Diego Baca <diegofbacag@gmail.com>
2026-03-04 16:13:23 -0500

    fix

--- front/kuntur-website/app/api/transactions/route.ts
+++ front/kuntur-website/app/api/transactions/route.ts
@@ -126,27 +126,3 @@ export async function POST(request: Request) {
     return NextResponse.json({ error: error }, { status: 500 })
   }
 }
-
-export async function DELETE(
-  _request: Request,
-  { params }: { params: { id: string } },
-) {
-  try {
-    const session = await getServerSession(authOptions)
-
-    if (!session?.user?.id) {
-      return new Response('Unauthorized', { status: 401 })
-    }
-
-    const id = params.id
-
-    await prisma.transaction.update({
-      where: { id, userId: session.user.id },
-      data: { deleted_at: new Date() },
-    })
-
-    return NextResponse.json({ success: true })
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 })
-  }
-}

4d4a7e23d0de11dcd2ccdbb6993b021481e8242f
Diego Baca <diegofbacag@gmail.com>
2026-03-04 14:46:20 -0500

    update services

--- front/kuntur-website/src/features/auth/services/auth.service.ts
+++ front/kuntur-website/src/features/auth/services/auth.service.ts
@@ -5,14 +5,14 @@ import { Credentials } from '../components/SignUpForm'
 
 export const emailSignUp = async (newUser: Credentials) => {
   const payload = newUser
-  const { data } = await axios.post(`${baseUrl}/auth/signup`, payload)
+  const { data } = await axios.post(`/api/auth/signup`, payload)
   console.log('emailSignUp service', data)
   return data
 }
 
 export const emailSignIn = async (credentials: Credentials) => {
   const payload = credentials
-  const { data } = await axios.post(`${baseUrl}/auth/signin`, payload)
+  const { data } = await axios.post(`/api/auth/signin`, payload)
   console.log('emailSignUp service', data)
   return data
 }
--- front/kuntur-website/src/features/expenses/services/expenses.service.ts
+++ front/kuntur-website/src/features/expenses/services/expenses.service.ts
@@ -5,20 +5,20 @@ import { mapTransactionDtoToTransaction } from '../mappers/transaction.mapper'
 import { baseUrl } from '@/lib/config'
 
 export const getExpenses = async () => {
-  const { data } = await axios.get(`${baseUrl}/transactions`)
+  const { data } = await axios.get(`/api/transactions`)
   console.log('getExpenses service: ', data)
   return data
 }
 
 export const createExpense = async (dto: CreateTransactionDto) => {
-  const { data } = await axios.post(`${baseUrl}/transactions`, dto)
+  const { data } = await axios.post(`/api/transactions`, dto)
   const transaction: TransactionDto = data.transaction
   return mapTransactionDtoToTransaction(transaction)
 }
 
 export const deleteExpense = async (id: string) => {
   const accessToken = localStorage.getItem('accessToken')
-  await axios.delete(`${baseUrl}/expenses/${id}`, {
+  await axios.delete(`/api/expenses/${id}`, {
     headers: { Authorization: `Bearer ${accessToken}` },
   })
 }
